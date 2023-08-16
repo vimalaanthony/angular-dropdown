1c53998ce497e612b8b559336cc5ee3603ebace3
vimalaanthony <vimalamca1991@gmail.com>
2023-08-16 19:38:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-dropdown
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-y1vbau)
---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h1>Hello from {{name}}!</h1>
+    <div class="container">
+     <div class="dropdown-header" (click)="openDropDown()">
+       <div class="title"> title</div>
+       <div class="arrow"> < </div>
+     </div>
+     <div class="dropdown-body" *ngIf="isShow">
+        <ng-container *ngFor="let opt of listItems">
+          <p> {{opt}}</p>
+        </ng-container>
+     </div>
+    <div>
+  `,
+})
+export class App {
+  name = 'Angular';
+  isShow: boolean = false;
+  listItems = ["option1", "option2", "option3", "option4", "option5", "option1"]
+
+  openDropDown() {
+    this.isShow = !this.isShow;
+  }
+}
+
+bootstrapApplication(App);
